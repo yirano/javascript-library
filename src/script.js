ac0ab87d1d48ab04e81b4973247485a331183218
yirano <tiffany.arellano@outlook.com>
2019-10-19 17:18:57 -0400

    Add status button

--- src/script.js
+++ src/script.js
@@ -6,9 +6,18 @@ let delete_card = document.querySelector('.delete');
 let cards = document.querySelector('.cards');
 let close = document.querySelector('.close');
 let inputs = document.querySelectorAll('input')
+let status = document.querySelector('.status button')
+let read = document.querySelector('#read');
 let library = [];
 let id_stat = 0;
 let evt_fired = false;
+
+let Books = function (title, author, status) {
+  this.title = title;
+  this.author = author;
+  this.status = status;
+  this.id_num = id_stat += 1;
+}
 
 function del(e) {
   document.getElementById(e.target.value).remove();
@@ -24,18 +33,29 @@ function drop(e) {
   evt_fired = true;
 }
 
-let Books = function (title, author, status) {
-  this.title = title;
-  this.author = author;
-  this.status = status;
-  this.id_num = id_stat += 1;
-}
-
-let add_book = function () {
+function add_book() {
   let new_title = document.getElementById('title').value;
   let new_author = document.getElementById('author').value;
   let new_book = new Books(new_title, new_author);
   library.push(new_book);
+}
+
+// function read_status() {
+//   if (read.checked == false) {
+//     status.classList.remove('read');
+//   } else {
+//     status.classList.add('read');
+//   }
+// }
+
+function submit_form() {
+  if (inputs[0].value != '' && inputs[1].value != '') {
+    add_book();
+    display();
+    for (input of inputs) {
+      input.value = '';
+    }
+  }
 }
 
 function display() {
@@ -44,10 +64,23 @@ function display() {
   let t = document.createElement('div');
   let a = document.createElement('div');
   let b = document.createElement('button');
+  let s = document.createElement('div');
+  let r = document.createElement('button');
   c.classList.add('card');
   t.classList.add('title');
+  s.classList.add('status');
+  r.classList.add('read');
+  r.textContent = 'Read';
   a.classList.add('author');
   b.classList.add('delete');
+  r.addEventListener('click', function (e) {
+    e.target.classList.toggle('read');
+    if (e.target.classList.contains('read')) {
+      e.target.textContent = 'Read';
+    } else {
+      e.target.textContent = 'To Read';
+    }
+  })
   b.addEventListener('click', function (e) {
     drop(e)
     setTimeout(function () {
@@ -63,11 +96,22 @@ function display() {
     b.value = id_stat;
     c.id = id_stat;
     c.appendChild(t);
+    c.appendChild(s);
     c.appendChild(a);
     c.appendChild(b);
+    s.appendChild(r);
     cards.appendChild(c);
   }
 }
+
+status.addEventListener('click', function (e) {
+  e.target.classList.toggle('read');
+  if (e.target.classList.contains('read')) {
+    e.target.textContent = 'Read';
+  } else {
+    e.target.textContent = 'To Read';
+  }
+})
 
 close.addEventListener('click', function () {
   form_wrapper.classList.add('hide');
@@ -90,15 +134,7 @@ form.addEventListener('keypress', function (e) {
   }
 })
 
-form_submit.addEventListener('click', function () {
-  if (inputs[0].value != '' && inputs[1].value != '') {
-    add_book();
-    display();
-    for (input of inputs) {
-      input.value = '';
-    }
-  }
-})
+form_submit.addEventListener('click', submit_form);
 
 delete_card.addEventListener('click', function (e) {
   drop(e)
